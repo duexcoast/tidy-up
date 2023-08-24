edc046972fb39e8deb336c241fe68806221af644
duexcoast <conor.ux@gmail.com>
2023-08-24 12:43:43 -0400

    Extract FiletypeSorter default values slice into separate file

--- pkg/tidy/tidy.go
+++ pkg/tidy/tidy.go
@@ -177,45 +177,7 @@ func (ftsf *FiletypeSortingFolder) String() string {
 }
 
 func NewFiletypeSorter() *FiletypeSorter {
-	dirs := []*FiletypeSortingFolder{
-		{
-			Name:       "Audio",
-			Extensions: []string{"aa", "aax", "act", "aiff", "alac", "au", "wav", "flac", "ra", "wma", "ac3", "m4b", "mp3", "aac", "ots"},
-		},
-		{
-			Name:       "Code",
-			Extensions: []string{"html", "js", "json", "ts", "tsx", "jsx", "go", "c", "cpp", "java", "awk", "sh", "zsh", "lua", "pl", "obj", "s", "sql", "py", "r", "rb", "rs", "cs", "kt", "php", "pm", "rkt", "rktl", "scm", "scala"},
-		},
-		{
-			Name:       "Compressed",
-			Extensions: []string{"a", "ar", "cpio", "shar", "lbr", "iso", "mar", "sbx", "tar", "br", "bz2", "f", "?xf", "genozip", "gz", "lz", "lz4", "lzma", "lzo", "rz", "sz", "sfark", "xz", "z", "zst", "7z", "s7z", "ace", "afa", "alz", "apk", "arc", "ark", "arc", "cdx", "arj", "b1", "b6z", "ba", "bh", "cab", "car", "cfs", "cpt", "dar", "dd", "dgc", "dmg", "ear", "gca", "genozip", "ha", "hki", "ice", "kgb", "lzh", "lha", "lzx", "pak", "partimg", "paq6", "paq7", "paq8", "pea", "phar", "pim", "pit", "qda", "rar", "rk", "sda", "sea", "sen", "sfx", "shk", "sit", "sitx", "sqx", "tar.gz", "tgz", "tar.z", "tar.bz2", "tbz2", "tar.lz", "tlz", "tar.xz", "txz", "tar.zst", "uc", "uc0", "uc2", "ucn", "ur2", "ue2", "uca", "uha", "war", "wim", "xar", "xp3", "yz1", "zip", "zipx", "zoo", "zpaq", "zz", "ecc", "ecsbx", "par", "par2", "rev"},
-		},
-		{
-			Name:       "Directories",
-			Extensions: []string{},
-		},
-		{
-			Name:       "Documents",
-			Extensions: []string{"doc", "docx", "odt", "msg", "rtf", "tex", "txt", "wks", "wps", "wpd", "md"},
-		},
-		{
-			Name:       "Images",
-			Extensions: []string{"jpeg", "jpg", "ai", "bmp", "gif", "heif", "heic", "ico", "max", "obj", "png", "ps", "psd", "svg", "tif", "tiff", "3ds", "3dm", "webp"},
-		},
-		{
-			Name:       "Other",
-			Extensions: []string{},
-		},
-		{
-			Name:       "PDFs",
-			Extensions: []string{"pdf"},
-		},
-		{
-			Name:       "Videos",
-			Extensions: []string{"avi", "flv", "h264", "m4v", "mkv", "mov", "mp4", "mpg", "mpeg", "mpeg-1", "mpeg-2", "mpeg-4", "", "rm", "swf", "vob", "wmv", "3g2", "3gp"},
-		},
-	}
-
+	dirs := defaultFiletypeSorter()
 	ftSorter := &FiletypeSorter{Dirs: dirs, logger: logger.Get()}
 	ftSorter.Lookup = ftSorter.newLookup()
 
