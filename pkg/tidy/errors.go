8be1e58021b9005d830d23a3cd6d875b6625d0ed
duexcoast <conor.ux@gmail.com>
2023-09-18 23:16:41 -0400

    define error value SortingError

--- pkg/tidy/errors.go
+++ pkg/tidy/errors.go
@@ -10,6 +10,13 @@ type SortingError struct {
 }
 
 func (se *SortingError) Error() string {
-	return fmt.Sprintf("Sorting Error: Could not move file to desired destination.\n\tFile:\t[%s]\n\tDest:\t[%s]\n\n\tError:\t%s",
-		se.Filename, se.AbsPath, se.Err.Error())
+	return fmt.Sprintf("Sorting Error: Could not move file to desired destination.\n\tFile:\t[%s]\n\tDest:\t[%s]\n\tOperation:\t[%s]\n\n\tError:\t%s",
+		se.Filename, se.AbsPath, se.sortOperation(), se.Err.Error())
+}
+
+func (se *SortingError) sortOperation() string {
+	if se.Sort {
+		return "Sort"
+	}
+	return "Unsort"
 }
